eb4ce97900e34cfa923caa2b3679e99a8e3fd027
someone <someone@example.com>
2020-07-20 13:59:59 +0800

    clear

--- src/main/java/com/example/rest/adw/OracleDS.java
+++ src/main/java/com/example/rest/adw/OracleDS.java
@@ -14,52 +14,52 @@ public class OracleDS {
 
 	// connect by wallet
 
-	// public static synchronized void init() throws SQLException {
-	// 	if (ds == null) {
-	// 		ds = new OracleDataSource();
-
-	// 		String connectString = Config.getValue("db.connectString");
-	// 		String walletName = Config.getValue("db.walletName");
-
-	// 		System.out.println("connectString=" + connectString);
-	// 		System.out.println("walletName=" + walletName);
-
-	// 		ds.setURL("jdbc:oracle:thin:@" + connectString + "?TNS_ADMIN=" + Config.CONFIG_HOME.getAbsolutePath() + "/" + walletName);
-	// 		ds.setUser(Config.getValue("db.username"));
-	// 		ds.setPassword(Config.getValue("db.password"));
-
-	// 		java.util.Properties pps = new java.util.Properties();
-	// 		pps.setProperty("InitialLimit", Config.getValue("db.initSize"));
-	// 		pps.setProperty("MinLimit", Config.getValue("db.minSize"));
-	// 		pps.setProperty("MaxLimit", Config.getValue("db.maxSize"));
-	// 		ds.setConnectionCacheProperties(pps);
-	// 		ds.setConnectionCachingEnabled(true);
-	// 		// warm up
-	// 		ds.getConnection().close();
-	// 	}
-	// }
-
-	// connect db system
-	// private static OracleDataSource ds = null;
-
 	public static synchronized void init() throws SQLException {
 		if (ds == null) {
 			ds = new OracleDataSource();
-			ds.setURL(Config.getValue("db.url"));
+
+			String connectString = Config.getValue("db.connectString");
+			String walletName = Config.getValue("db.walletName");
+
+			System.out.println("connectString=" + connectString);
+			System.out.println("walletName=" + walletName);
+
+			ds.setURL("jdbc:oracle:thin:@" + connectString + "?TNS_ADMIN=" + Config.CONFIG_HOME.getAbsolutePath() + "/" + walletName);
 			ds.setUser(Config.getValue("db.username"));
 			ds.setPassword(Config.getValue("db.password"));
 
 			java.util.Properties pps = new java.util.Properties();
-			pps.setProperty("InitialLimit", Config.getValue("db.initSize"));    
-			pps.setProperty("MinLimit", Config.getValue("db.minSize"));    
-			pps.setProperty("MaxLimit", Config.getValue("db.maxSize"));    
+			pps.setProperty("InitialLimit", Config.getValue("db.initSize"));
+			pps.setProperty("MinLimit", Config.getValue("db.minSize"));
+			pps.setProperty("MaxLimit", Config.getValue("db.maxSize"));
 			ds.setConnectionCacheProperties(pps);
 			ds.setConnectionCachingEnabled(true);
-		    // warm up
-			ds.getConnection();
-			
+			// warm up
+			ds.getConnection().close();
 		}
 	}
+
+	// connect db system
+	// private static OracleDataSource ds = null;
+
+	// public static synchronized void init() throws SQLException {
+	// 	if (ds == null) {
+	// 		ds = new OracleDataSource();
+	// 		ds.setURL(Config.getValue("db.url"));
+	// 		ds.setUser(Config.getValue("db.username"));
+	// 		ds.setPassword(Config.getValue("db.password"));
+
+	// 		java.util.Properties pps = new java.util.Properties();
+	// 		pps.setProperty("InitialLimit", Config.getValue("db.initSize"));    
+	// 		pps.setProperty("MinLimit", Config.getValue("db.minSize"));    
+	// 		pps.setProperty("MaxLimit", Config.getValue("db.maxSize"));    
+	// 		ds.setConnectionCacheProperties(pps);
+	// 		ds.setConnectionCachingEnabled(true);
+	// 	    // warm up
+	// 		ds.getConnection();
+			
+	// 	}
+	// }
 
 	public Connection getConnection() throws SQLException {
 		if (ds == null) {
